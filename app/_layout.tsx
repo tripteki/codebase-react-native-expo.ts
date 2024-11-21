6c4ca71b542ff0d336d910803a7247379c5b8d97
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-11-21 18:05:38 +0700

    feat(view): Paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 "use strict";
 
 import { FC, } from "react";
-import { Text, } from "react-native";
+import { SafeAreaView, } from "react-native";
+import { PaperProvider, Button, } from "react-native-paper";
 import i18n from "@/app-i18n.config";
 
 const Template: FC = () =>
 {
     return (
 
-        <Text>{i18n.t ("common.welcome")}</Text>
+        <PaperProvider>
+            <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center", }}>
+                <Button icon="account" mode="contained" onPress={() => {}}>
+                    {i18n.t ("common.welcome")}
+                </Button>
+            </SafeAreaView>
+        </PaperProvider>
     );
 };
 
---
+++ babel.config.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = function (api) {
+
+    api.cache (true);
+
+    return {
+
+        presets: [
+
+            "babel-preset-expo",
+        ],
+
+        env: {
+
+            production: {
+
+                plugins: [
+
+                    "react-native-paper/babel",
+                ],
+            },
+        },
+    };
+};
